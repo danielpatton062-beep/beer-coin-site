21a47af68b184a49108b7fd61faea3485d9fd2d4
danielpatton062-beep <danielpatton062@gmail.com>
2026-03-24 11:55:40 -0500

    Add Tailwind CSS configuration with amber color

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        amber: {
+          50: '#fff8e5',
+          400: '#f59e0b',
+          500: '#d97706'
+        }
+      }
+    }
+  },
+  plugins: []
+};
