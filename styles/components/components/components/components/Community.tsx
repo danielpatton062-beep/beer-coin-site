57960617d2e3cb2ec015e37fe0533ca764e6287b
danielpatton062-beep <danielpatton062@gmail.com>
2026-03-24 12:05:06 -0500

    Create Community.tsx

---
+++ styles/components/components/components/components/Community.tsx
@@ -0,0 +1,13 @@
+export default function Community() {
+  return (
+    <section id="community" className="py-12 bg-white text-center">
+      <h2 className="text-3xl font-bold text-amber-700 mb-4">Join the Community</h2>
+      <p className="text-gray-700 mb-6">Grab a beer, share memes, and join the fun 🍻</p>
+      <div className="flex justify-center gap-5 text-amber-600 font-semibold">
+        <a href="[facebook.com](https://www.facebook.com/share/1Fn8geWPgJ/)" target="_blank">Facebook</a>
+        <a href="[x.com](https://x.com/DanielPatton47)" target="_blank">X / Twitter</a>
+        <a href="[music.youtube.com](https://music.youtube.com/playlist?list=PLmtnqAMd-KYI7Axn02xZh0-d2F50NBj4j&si=xbkSf_tGslX5tqgI)" target="_blank">YouTube Music</a>
+      </div>
+    </section>
+  );
+}
